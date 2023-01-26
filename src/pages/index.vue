83923d944a6dee4b3842732aa3703e756dff7150
Chase Isley <isleychase@gmail.com>
2023-01-26 14:25:25 -0800

    added project

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
   import Typewriter from '~/components/ui/typewriter/typewriter.vue';
-  import { skills } from '~/static/data';
+  import Card from '~/components/ui/card/Card.vue';
+  import { projects, skills } from '~/static/data';
 </script>
 
 <template>
   <h1>Hi, I'm Chase Isley</h1>
   <p class="text-sm mb-4">Software Engineer</p>
-  <p class="text-lg mb-2">Some of my skills include:</p>
 
-  <Typewriter :data="skills" />
+  <Typewriter :data="skills" class="mb-12" />
+
+  <h2 class="mb-4">Latest Projects</h2>
+
+  <div class="grid md:grid-cols-2 gap-4">
+    <a
+      v-for="{ description, title, url, gradient } in projects"
+      :key="title"
+      class="transform hover:scale-105 transition-all rounded-xl w-full bg-gradient-to-r p-1 custom-link"
+      :class="gradient"
+      :href="url"
+      :title="title"
+      target="_blank"
+    >
+      <Card class="h-full dark:bg-black">
+        <div class="font-semibold text-lg dark:text-white">
+          {{ title }}
+        </div>
+        <div v-if="description" class="mt-2 text-sm">
+          {{ description }}
+        </div>
+      </Card>
+    </a>
+  </div>
 </template>
