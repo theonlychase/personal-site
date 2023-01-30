ad5d574101f9100024c892046bfe91e17e163e12
Chase Isley <isleychase@gmail.com>
2023-01-30 09:57:37 -0800

    update head

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  useHead({
+    title: 'Chase Isley - Contact',
+    link: [
+      {
+        rel: 'canonical',
+        href: 'https://chaseisley.dev/contact',
+      },
+    ],
+  });
+</script>
 
 <template>
   <h1 class="mb-12">Contact</h1>
