668bb35754264834bd22b96bcf61cc0a655b0fea
Chase Isley <isleychase@gmail.com>
2023-01-31 12:20:27 -0800

    fix when no blog found

--- src/pages/blog/[...slug].vue
+++ src/pages/blog/[...slug].vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
   const { path } = useRoute();
   const { data } = await useAsyncData('blog', () =>
-    $fetch(`/api/_content/query?path=${path}`),
+    $fetch(`/api/_content/query?_params={"where":{"_path":"${path}"}}`),
   );
+
+  // @ts-ignore
+  const { title, description, _path } = data?.value[0] ?? {};
+
+  useHead({
+    title: `Chase Isley - ${title}`,
+    meta: [
+      {
+        name: 'description',
+        content: `${description}`,
+      },
+    ],
+    link: [
+      {
+        rel: 'canonical',
+        href: `https://chaseisley.dev${_path}`,
+      },
+    ],
+  });
 </script>
 
 <template>
